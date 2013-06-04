9b0a4cfede5e3ecbe136a7765e7ee2f08d0b7e17
takatsugu ishikawa <t_ishikawa@zenrin-datacom.net>
2013-06-04 20:05:06 +0900

    webViewを追加

--- SampleWebView/src/main/java/simul/samplewebview/MainActivity.java
+++ SampleWebView/src/main/java/simul/samplewebview/MainActivity.java
@@ -3,6 +3,11 @@ package simul.samplewebview;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView myWebView = (WebView)findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://google.co.jp/");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                myWebView.loadUrl("http://google.co.jp");
+            }
+        });
     }
 
 
